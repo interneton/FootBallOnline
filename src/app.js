e1adf817247257b8fcd9b15d6f0da348d47a3d69
interneton <78246370+interneton@users.noreply.github.com>
2024-09-19 12:05:05 +0900

    revert

--- src/app.js
+++ src/app.js
@@ -1,16 +1,10 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
-
-dotenv.config();
 
 const app = express();
+
 app.use(express.json());
-app.use(cookieParser());
 
-// 라우트 설정
-app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(3000, () => {
+  console.log(`포트 3000 서버가 시작`);
+});
--- src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { registerUser, loginUser } from '../controllers/userController.js';
-
-const router = express.Router();
-
-router.post('/register', registerUser); // 회원가입
-router.post('/login', loginUser);       // 로그인 (Access/Refresh Token 발급)
-
-export default router;
