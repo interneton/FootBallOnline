29c0d11349c0a7ed4489056b3b8aadd61939c5f3
gudrn4616 <128803700+gudrn4616@users.noreply.github.com>
2024-09-20 20:33:02 +0900

    Delete src/routers/authRouter.js

--- src/routers/authRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { login, signup } from '../controllers/authController.js';
-
-const router = Router();
-
-router.post('/login', login);
-router.post('/signup', signup);
-
-export default router;
