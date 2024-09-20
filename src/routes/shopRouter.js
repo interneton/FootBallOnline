502cd873f4c439c01384adb81049441cd27dd98f
interneton <78246370+interneton@users.noreply.github.com>
2024-09-20 21:08:53 +0900

    shop validationSchemas 적용

--- src/routes/shopRouter.js
+++ src/routes/shopRouter.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { purchaseCash, getCashAmount, drawPlayer, makePackcontroller } from "../controllers/shopController.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
+import { validateRequest } from '../middlewares/validationMiddleware.js';
+import { purchaseCashSchema, drawPlayerSchema } from '../schemas/validationSchemas.js'; // 스키마 경로
 
 const router = Router();
 
-router.post('/purchase',authMiddleware, purchaseCash);
+router.post('/purchase',authMiddleware, validateRequest(purchaseCashSchema), purchaseCash);
 router.get('/getCashAmount',authMiddleware, getCashAmount);
-router.post('/draw',authMiddleware, drawPlayer);  // 선수 뽑기 기능 추가
+router.post('/draw',authMiddleware, validateRequest(drawPlayerSchema), drawPlayer);  // 선수 뽑기 기능 추가
 router.post('/makepack',makePackcontroller)// 팩 만들기
 
 export default router;
