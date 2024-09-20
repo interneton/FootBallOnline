7203d946a22f3a76a6138f04f5d18e7f8dc50681
INU-coder <tothel56@gmail.com>
2024-09-20 20:28:39 +0900

    allDelete

--- src/routes/shopRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { drawPlayer } from '../controllers/shopGacha.js';
-
-const router = Router();
-
-// 선수 뽑기 (가챠) 라우터
-router.post('/draw', drawPlayer);
-
-export default router;
