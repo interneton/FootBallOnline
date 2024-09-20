e665212b8c7e6e9c38003ccbbbdfe6b1d869c910
gudrn4616 <128803700+gudrn4616@users.noreply.github.com>
2024-09-20 20:32:56 +0900

    Delete src/routers/gameRouter.js

--- src/routers/gameRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { playGame, getRanking } from '../controllers/gameController.js';
-
-const router = Router();
-
-router.post('/play', playGame);
-router.get('/ranking', getRanking);
-
-export default router;
