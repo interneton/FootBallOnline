c809735ac6709ba6ddc4e8f4b14c757aace4f034
interneton <78246370+interneton@users.noreply.github.com>
2024-09-20 21:10:29 +0900

    auth middleware import 수정

--- src/routes/shopRouter.js
+++ src/routes/shopRouter.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { purchaseCash, getCashAmount, drawPlayer, makePackcontroller } from "../controllers/shopController.js";
-import authMiddleware from "../middlewares/auth.middleware.js";
+import { authenticateToken } from "../middlewares/authMiddleware.js";
 import { validateRequest } from '../middlewares/validationMiddleware.js';
 import { purchaseCashSchema, drawPlayerSchema } from '../schemas/validationSchemas.js'; // 스키마 경로
 
 const router = Router();
 
-router.post('/purchase',authMiddleware, validateRequest(purchaseCashSchema), purchaseCash);
-router.get('/getCashAmount',authMiddleware, getCashAmount);
-router.post('/draw',authMiddleware, validateRequest(drawPlayerSchema), drawPlayer);  // 선수 뽑기 기능 추가
+router.post('/purchase',authenticateToken, validateRequest(purchaseCashSchema), purchaseCash);
+router.get('/getCashAmount',authenticateToken, getCashAmount);
+router.post('/draw',authenticateToken, validateRequest(drawPlayerSchema), drawPlayer);  // 선수 뽑기 기능 추가
 router.post('/makepack',makePackcontroller)// 팩 만들기
 
 export default router;
