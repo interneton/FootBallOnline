4c2662eff65d32201d297da22f8d4fbed0b5ab6d
SUNJOON96 <sunjoon1300@naver.com>
2024-09-23 23:22:03 +0900

    HSJ_commit777
    tokenRouter.js, protectedRouter.js
    userRouter.js로 병합 및 app.js 수정

--- src/routes/tokenRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { refreshAccessToken } from '../controllers/tokenController.js';
-
-const router = Router();
-
-router.post('/refresh-token', refreshAccessToken); 
-
-export default router;
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import { login, signup } from '../controllers/userController.js';
+import { refreshAccessToken } from '../controllers/tokenController.js';
+import { authenticateToken } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 router.post('/login', login);    // 로그인
 router.post('/signup', signup);  // 회원가입
+router.post('/refresh-token', refreshAccessToken); // 토큰 갱신
+router.get('/protected-endpoint', authenticateToken, (req, res) => {
+    const userId = req.user.userId;  // 인증된 사용자의 userId를 이용
+    res.status(200).json({ message: `사용자 ${userId}님, 보호된 리소스에 접근하였습니다.` });
+});
 
 export default router;
