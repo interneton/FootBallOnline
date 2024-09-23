4162d1f84768982df33491f359d1fc4b86a928b1
interneton <78246370+interneton@users.noreply.github.com>
2024-09-23 19:27:17 +0900

    userController next 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,17 @@
 import { signupService, loginService, createAccessToken, createRefreshToken } from '../services/userService.js';
+import { CustomError } from '../utils/customError.js'; 
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   try {
     const { account, password, name, teamName } = req.body;
     const newUser = await signupService(account, password, name, teamName);
     res.status(201).json({ message: '회원가입 성공', newUser });
   } catch (error) {
-    res.status(500).json({ message: '회원가입 실패', error });
+    next(new CustomError('회원가입 실패: ' + error.message, 500));
   }
 };
 
-export const login = async (req, res) => {
+export const login = async (req, res, next) => {
   try {
     const { account, password } = req.body;
     const user = await loginService(account, password);
@@ -20,6 +21,6 @@ export const login = async (req, res) => {
 
     res.status(200).json({ message: '로그인 성공', accessToken, refreshToken });
   } catch (error) {
-    res.status(500).json({ message: '로그인 실패', error });
+    next(new CustomError('로그인 실패: ' + error.message, 500));
   }
 };
